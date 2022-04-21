d78c59304b890eefd21d4997c2e61b35fddc0ca0
Musharrafbegim <70473841+Musharrafbegim@users.noreply.github.com>
2022-04-21 07:51:04 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14002,7 +14002,7 @@ function parallaxAnimation() {
 	setPin("#element-4").
 	addTo(controller);
 	
-	var scene = new ScrollMagic.Scene({ triggerElement: "#trigger-5", offset: -(0, _jquery2.default)('#element-4').height() + 20, duration: (0, _jquery2.default)('.contact-form').offset().top - (0, _jquery2.default)('#element-5').offset().top + (0, _jquery2.default)('.contact-form').height() - (0, _jquery2.default)(window).width() * (192 / 1920) - 10 }).
+	var scene = new ScrollMagic.Scene({ triggerElement: "#trigger-5", offset: -(0, _jquery2.default)('#element-4').height() + 20, duration: (0, _jquery2.default)('.contact-form').offset().top - (0, _jquery2.default)('#element-5').offset().top + (0, _jquery2.default)('.contact-form').height() - (0, _jquery2.default)(window).width() * (192 / 1920) - 5 }).
 	setPin("#element-5").
 	addTo(controller);
 
